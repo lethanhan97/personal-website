86607990ad86e66cf358c71e1503b142ba71a6dd
Le Thanh An <an@stashaway.com>
2024-07-25 20:49:08 +0700

    feat: add layout

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,12 +7,47 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+interface HomeBoxProps {
+  children: React.ReactNode;
+  className?: string;
+}
+function HomeBox({ children, className }: HomeBoxProps) {
+  return (
+    <div
+      className={`bg-gray-400 rounded-lg flex items-center justify-center ${className}`}
+    >
+      {children}
+    </div>
+  );
+}
+
 export default function Index() {
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">an. personal website</h1>
-
-      <p>Cool things will come</p>
+    <div className="font-sans p-4 bg-gray-200 w-[100vw] h-[100vh] flex flex-row">
+      <section className="flex-1 items-stretch flex flex-col">
+        <HomeBox className="flex-[2] mb-4">
+          <p>Side Box 1</p>
+        </HomeBox>
+        <HomeBox className="flex-1">
+          <p>Side Box 2</p>
+        </HomeBox>
+      </section>
+      <section className="flex-1 items-stretch mx-4">
+        <HomeBox className="h-[100%] flex-col">
+          <div className="text-center">
+            <h1 className="text-xl font-semibold">an. personal website</h1>
+            <p>cool things will come</p>
+          </div>
+        </HomeBox>
+      </section>
+      <section className="flex-1 items-stretch flex flex-col">
+        <HomeBox className="flex-1">
+          <p>Side Box 1</p>
+        </HomeBox>
+        <HomeBox className="flex-[2] mt-4">
+          <p>Side Box 2</p>
+        </HomeBox>
+      </section>
     </div>
   );
 }
